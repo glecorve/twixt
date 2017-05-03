6b68ec251514eaaef7ef6f79de7578887514830a
Gwénolé Lecorvé <gwenole.lecorve@irisa.fr>
2017-05-03 15:06:08 +0200

    Retour à une version fonctionnelle

--- src/Twixt.java
+++ src/Twixt.java
@@ -26,31 +26,33 @@ public class Twixt {
     	int boardSize = 12;
     	
     	GameFrame g = new GameFrame(boardSize);
-		LogicManager logicManager = g.getLogicManager();
-    	RunningGame runningGame = new RunningGame(g.getLogicManager(), g);
+//		LogicManager logicManager = g.getLogicManager();
+//    	RunningGame runningGame = new RunningGame(logicManager, g);
+//    	RunningGame runningGame = new RunningGame(g.getLogicManager(), g);
+//		runningGame.setPlayers(g, g);
+//		runningGame.start();
 
-
-		// No argument => GUI
-		if (args.length == 0)
-		{
-	    	Player player1 = g; // Human player plays through GUI
-	    	Player player2 = g; // Human player plays through GUI
-	    	runningGame.setPlayers(player1, player2);
-	    	runningGame.run();
-		}
-		// Need to 2 arguments
-		else if (args.length == 2)
-		{
-			Factory  f_ai = new Factory();
-			Player player1 = f_ai.getAI((String) args[0]);
-			Player player2 = f_ai.getAI((String) args[1]);
-			runningGame.setPlayers(player1, player2);
-			runningGame.start();  // FIXME: start
-		}
-		else
-		{
-			System.err.println("twixt [<IA joueur 1> <IA joueur 2>]");
-		}
+//		// No argument => GUI
+//		if (args.length == 0)
+//		{
+//	    	Player player1 = g; // Human player plays through GUI
+//	    	Player player2 = g; // Human player plays through GUI
+//	    	runningGame.setPlayers(player1, player2);
+//	    	runningGame.start();
+//		}
+//		// Need to 2 arguments
+//		else if (args.length == 2)
+//		{
+//			Factory  f_ai = new Factory();
+//			Player player1 = f_ai.getAI((String) args[0]);
+//			Player player2 = f_ai.getAI((String) args[1]);
+//			runningGame.setPlayers(player1, player2);
+//			runningGame.start();  // FIXME: start
+//		}
+//		else
+//		{
+//			System.err.println("twixt [<IA joueur 1> <IA joueur 2>]");
+//		}
     }
 
 }
